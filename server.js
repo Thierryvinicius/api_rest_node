82572f70b6f8a4fa4fd8483500665062a16a47c1
Thierry Lima <thierry.vinicius@outlook.com>
2025-04-05 16:05:52 -0400

    Melhorando a api e fazendo conexão com mongodb

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+app.post("/usuarios", async (req, res) => {
+  await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  });
+
+  res.status(201).json(req.body);
+});
+
+app.get("/usuarios", async (req, res) => {
+  const users = await prisma.user.findMany();
+
+  res.status(200).json(users);
+});
+
+app.listen(PORT);
